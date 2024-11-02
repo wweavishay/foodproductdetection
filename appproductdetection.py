58beef174f7368245f491832f10c4f5bc41cc965
wweavishayaknin <WWEAVISHAYAKNIN@GMAIL.COM>
2024-11-02 19:20:29 +0200

    Update appproductdetection.py

--- appproductdetection.py
+++ appproductdetection.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:962436fd4d390846fe13f740e5d362a69fc9af2e4f96bb4a11823367c070f2a2
-size 2842
+oid sha256:6af327055bcd138e940b181cea08ba6b7cddf41a3719de353428dbc4c57d6c50
+size 8021
